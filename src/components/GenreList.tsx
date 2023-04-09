e99978fea3ade79d1d402e518f14578aa8ee30ac
FdgCertainly <94852583+FdgCertainly@users.noreply.github.com>
2023-04-10 01:15:48 +0200

    Show spinner while fetching genres, adjust dims

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,22 @@
-import useGenres from "../hooks/useGenres";
-
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react"
+import useGenres from "../hooks/useGenres"
+import getCroppedImageUrl from "../services/image-url"
 
 export default function GenreList() {
-    const {data, error, loading} = useGenres();
+  const { data, error, loading } = useGenres()
+
+  if (error) return null
+  if (loading) return <Spinner/>
   return (
-    <ul>
-        {
-            data.map(d => <li key={d.id}>{d.name}</li>)
-        }
-    </ul>
+    <List>
+      {data.map((d) => (
+        <ListItem key={d.id}>
+            <HStack paddingY={1}>
+                <Image src={getCroppedImageUrl(d.image_background)} boxSize='32px' borderRadius={8} />
+                <Text fontSize='lg'>{d.name}</Text>
+            </HStack>
+        </ListItem>
+      ))}
+    </List>
   )
 }
